e1ba558a053999f8e6cea715cc034abf00f3a7ae
Arnav Jain <arnavkj11@gmail.com>
2025-03-24 11:28:09 -0500

    restart

--- main.py
+++
@@ -1,5 +0,0 @@
-from views import *
-from models import *
-from mini_web import run
-
-run()
--- views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .index import home, about
-from .users import signup_view
-from .appointments import schedule_appointment_view
